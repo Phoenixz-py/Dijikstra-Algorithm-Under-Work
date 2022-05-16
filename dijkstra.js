5bf09100375284e6637714458eaff39106379a8e
Fenguin Phoenix <73833580+Phoenixz-py@users.noreply.github.com>
2022-05-17 00:41:15 +0530

    Update dijkstra.js

--- dijkstra.js
+++ dijkstra.js
@@ -37,32 +37,33 @@ let Destination = prompt(goTo)
 
 // Graph is nothing but group of objetcs
 
-class Edge{
+class EDGE{
     constructor(startVertex, endVertex, weight){
         this.startVertex = startVertex;
         this.endVertex = endVertex;
         this.weight = weight;
     }
-    dijkstra(){
-        let displayer = `The starting vertexes : + ${this.startVertex}\n + ${this.endVertex}\n + ${this.weight}`;
-        console.table(displayer)
-    }
-}
-
-class transverse extends Edge {
-
-   myMethod(){
-       return super.dijkstra() + ' Lets see if it works lol ';
-   }
-
+    // dijkstra(){
+    //     let displayer = `The starting vertexes : + ${this.startVertex}\n + ${this.endVertex}\n + ${this.weight}`;
+    //     console.table(displayer)
+    // }
 }
 
 
-let edges = [new Edge(Edge1, Edge2, wOfedge12), new Edge(Edge2, Edge3, wOfedge23), new Edge(Edge3, Edge4, wOfedge34), new Edge(Edge4, Edge1, wofedge41)];
+// class TRANSVERSE extends EDGE {
+
+//    myMethod(){
+//        return super.dijkstra() + ' Lets see if it works lol ';
+//    }
+
+// }
+
+
+let edges = [new EDGE(Edge1, Edge2, wOfedge12), new EDGE(Edge2, Edge3, wOfedge23), new EDGE(Edge3, Edge4, wOfedge34), new EDGE(Edge4, Edge1, wofedge41)];
 // let edges = [new Edge(Edge1, Edge2, wOfedge12), new Edge(Edge2, Edge3, wOfedge23), new Edge(Edge3, Edge4, wOfedge34), new Edge(Edge4, Edge5, wOfedge45), new Edge(Edge5, Edge6, wOfedge56), new Edge(Edge6, Edge1, wOfedge61)];
 
-const startPoint = Edge1
-// const endPoint = Edge6
+// const startPoint = Edge1
+// // const endPoint = Edge6
 
 let startOnEdge = edges.filter(e => e.startVertex == Edge1);
 
@@ -79,4 +80,4 @@ for(edges of startOnEdge){
     }
 }
 
-console.log((new transverse()).myMethod());
+console.table(edges)
